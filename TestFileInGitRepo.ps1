d595e27044fc7db6c8c2db27e27bbb5aedd2e8ff
John Laerum <john.laerum@cornerstone.se>
2022-06-29 18:38:49 +0200

    I guess there was a previous commit message, but I have no idea what it was

--- TestFileInGitRepo.ps1
+++ TestFileInGitRepo.ps1
@@ -2,3 +2,4 @@
 # This is the first edit to the file
 # This is the second edit to the file (still in local repo)
 # This is a test of staging a change
+# This file is not staged for a change where 3 files were changed
